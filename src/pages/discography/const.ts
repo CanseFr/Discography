c1e2459e6ec3e16c841786d00019d0ef445a324a
CanseFr <jucansell@gmail.com>
2024-12-16 21:11:25 +0100

    :sparkles: Create disco display

---
+++ src/pages/discography/const.ts
@@ -0,0 +1,123 @@
+export const discographyLinks = [
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/still-pushing-the-vibes",
+        title: "Still Pushing The Vibes by Canse",
+        album: "/album=562430046",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/sun-reboot",
+        title: "Sun Reboot by Canse",
+        album: "/album=3542979607",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/g-powa",
+        title: "G-Powa by Canse",
+        album: "/album=487315781",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/e-36",
+        title: "E-36 by Canse",
+        album: "/album=857039024",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/the-chronic",
+        title: "The Chronic by Canse",
+        album: "/album=3583639476",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/all-eyez-on-us",
+        title: "All Eyez On Us by Canse",
+        album: "/album=1751949913",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/momentary-lapse",
+        title: "Momentary Lapse by Canse",
+        album: "/album=3336907893",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/about-all",
+        title: "About All by Canse",
+        album: "/album=492184313",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/nksmr",
+        title: "NKSMR by Canse",
+        album: "/album=2105994787",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/physical-abstraction",
+        title: "Physical Abstraction by Canse",
+        album: "/album=151482061",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/music-about-nothing",
+        title: "Music About Nothing by Canse",
+        album: "/album=1683269806",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/cheese-tape",
+        title: "Cheese Tape by Canse",
+        album: "/album=2112476383",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/lords",
+        title: "Lords by Canse",
+        album: "/album=2785140083",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/exoplan-te",
+        title: "Exoplanète by Canse",
+        album: "/album=3557300870",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/shit-singing",
+        title: "Shit Singing by Canse",
+        album: "/album=2177609903",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/shit-stuff-1",
+        title: "Shit Stuff #1 by Canse",
+        album: "/album=3248398383",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/shit-stuff-2",
+        title: "Shit Stuff #2 by Canse",
+        album: "/album=1094413276",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/sexclub",
+        title: "SexClub by Canse",
+        album: "/album=2657806049",
+    },
+    {
+        src: "https://bandcamp.com/EmbeddedPlayer",
+        href: "https://canse.bandcamp.com/album/shit-stuff-3",
+        title: "Shit Stuff #3 by Canse",
+        album: "/album=3527667398",
+    },
+];
+
+export const size = "/size=large"
+export const bgcol = `/bgcol=000000`
+export const linkcol = "/linkcol=e99708"
+export const tracklist = "/tracklist=false"
+export const transparent = "/transparent=true"
+
--- src/pages/discography/index.tsx
+++ src/pages/discography/index.tsx
@@ -1,5 +1,25 @@
+import {bgcol, discographyLinks, linkcol, size, tracklist, transparent} from "./const.ts";
+import {Grid2, useTheme} from "@mui/material";
+// import "./index.css"
+
 export const DiscographyPage = () => {
+
+    const theme = useTheme();
+
     return (
-        <p>disco</p>
-    )
+        <Grid2 container style={{backgroundColor: theme.palette.background.default, display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center', paddingTop: '50px'}}>
+            {discographyLinks.map((link) => (
+                <Grid2 key={link.title} size={4} width="350px" height='470px'>
+                    <iframe
+                        style={{border: '0', width: '100%', height: '100%'}}
+                        src={`${link.src}${link.album}${size}${bgcol}${linkcol}${tracklist}${transparent}`}
+                        seamless
+                        title={link.title}
+                    >
+                        <a href={`${link.href}`}>{link.title}</a>
+                    </iframe>
+                </Grid2>
+            ))}
+        </Grid2>
+    );
 }
