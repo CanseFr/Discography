9dac0e035a7669d21e01e5db21a041536f8dc685
CanseFr <jucansell@gmail.com>
2024-12-16 16:36:15 +0100

    :recycle: Upgrade list page

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,5 +1,5 @@
-// import NightlightIcon from '@mui/icons-material/Nightlight';
-// import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,13 +7,22 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import {Grid2, Switch} from "@mui/material";
-import FormControlLabel from '@mui/material/FormControlLabel';
 import {useColorScheme} from '@mui/material/styles';
 import {Outlet} from "react-router";
 import {ChangeEvent} from "react";
 
-const pages = ['Discographie', 'Sample Pack', 'Service', 'About'];
-const link = ['disco', 'sample', 'service', 'about'];
+interface LinkAndLabelType {
+    label: string;
+    link: string;
+}
+
+const linkAndLabel: LinkAndLabelType[] = [
+    {label: 'Discography', link: 'disco'},
+    {label: 'Sample Pack', link: 'sample'},
+    {label: 'Service', link: 'service'},
+    {label: 'About', link: 'about'},
+]
+
 
 export const TopBar = () => {
 
@@ -23,7 +32,7 @@ export const TopBar = () => {
         return e.target.checked ? "dark" : "light"
     }
 
-    const handleSetTheme = (e:  ChangeEvent<HTMLInputElement>) => {
+    const handleSetTheme = (e: ChangeEvent<HTMLInputElement>) => {
         setMode(returnTheme(e))
     }
 
@@ -58,19 +67,24 @@ export const TopBar = () => {
                         </Grid2>
                         <Grid2>
                             <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
-                                {pages.map((page, index) => (
+                                {linkAndLabel.map((item) => (
                                     <Button
-                                        key={page}
+                                        key={item.label}
                                         sx={{my: 2, color: 'white', display: 'block'}}
-                                        href={link[index]}
+                                        href={item.link}
                                     >
-                                        {page}
+                                        {item.label}
                                     </Button>
                                 ))}
                             </Box>
                         </Grid2>
-                        <Grid2>
-                            <FormControlLabel control={<Switch onChange={(e) => handleSetTheme(e)} checked={mode === "dark"}/>} label="Theme"/>
+                        <Grid2 container spacing={2} alignItems="center">
+                            <Grid2>
+                                <Switch onChange={(e) => handleSetTheme(e)} checked={mode === "dark"}/>
+                            </Grid2>
+                            <Grid2>
+                                {mode === "dark" ? <NightlightIcon/> : <WbSunnyIcon/>}
+                            </Grid2>
                         </Grid2>
                     </Toolbar>
                 </Container>
