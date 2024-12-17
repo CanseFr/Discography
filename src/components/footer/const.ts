d94f5e1d9d8385721154ec61d10aed5e31c0fcde
CanseFr <jucansell@gmail.com>
2024-12-17 23:30:00 +0100

    :zap: Upgrade home bg and add assets

--- src/components/footer/const.ts
+++ src/components/footer/const.ts
@@ -1,10 +1,10 @@
 export const socialList = [
-    {link: "https://www.instagram.com/canse_fr", pic: "pictures/social/instagram.png"},
-    {link: "https://www.youtube.com/channel/UC8yFlykOSxvBv3UsZXsH9jg?view_as=subscriber", pic: "pictures/social/youtube.png"},
-    {link: "https://soundcloud.com/canse_fr", pic: "pictures/social/soundcloud.png"},
-    {link: "https://canse.bandcamp.com/", pic: "pictures/social/bandcamp.png"},
-    {link: "https://www.facebook.com/cansefrr/", pic: "pictures/social/facebook.png"},
-    {link: "https://github.com/CanseFr", pic: "pictures/social/path.png"},
-    {link: "https://canseshop.myspreadshop.fr/", pic: "pictures/social/shopping.png"},
-    {link: "https://mackerelclub.com/", pic: "pictures/social/internet.png"}
+    {link: "https://www.instagram.com/canse_fr", pic: `pictures/social/instagram.png`},
+    {link: "https://www.youtube.com/channel/UC8yFlykOSxvBv3UsZXsH9jg?view_as=subscriber", pic: `pictures/social/youtube.png`},
+    {link: "https://soundcloud.com/canse_fr", pic: `pictures/social/soundcloud.png`},
+    {link: "https://canse.bandcamp.com/", pic: `pictures/social/bandcamp.png`},
+    {link: "https://www.facebook.com/cansefrr/", pic: `pictures/social/facebook.png`},
+    {link: "https://github.com/CanseFr", pic: `pictures/social/path.png`},
+    {link: "https://canseshop.myspreadshop.fr/", pic: `pictures/social/shopping.png`},
+    {link: "https://mackerelclub.com/", pic: `pictures/social/internet.png`}
 ]
