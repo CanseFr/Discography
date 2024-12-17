bd594254820d685034e9659dc373e911d9210f37
CanseFr <jucansell@gmail.com>
2024-12-17 13:47:14 +0100

    :lipstick: Upgrade primary color

--- src/common/theme/index.ts
+++ src/common/theme/index.ts
@@ -1,8 +1,14 @@
 import {createTheme} from "@mui/material";
+import { lime } from '@mui/material/colors';
 
 export const theme = createTheme({
     colorSchemes: {
         dark: true,
+    },
+    palette:{
+        primary: {
+            main: lime[500]
+        }
     },
     components: {
         MuiTypography: {
